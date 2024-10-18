1e7dd261ac088dd4765a8c31f59f50152c03b7f5
callmePluto <anucha.se@rmuti.ac.th>
2024-10-18 17:16:28 +0700

    เพิ่ม Table 📇

---
+++ database/migrations/2024_10_18_101136_title.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('titles', function (Blueprint $table) {
+            $table->id('title_id');
+            $table->string('title_name');
+            $table->timestamp('created_by')->nullable();;
+            $table->timestamp('updated_by')->nullable();;
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
