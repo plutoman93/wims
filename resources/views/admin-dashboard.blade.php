4c79d028d6d64cfaa7d46d662cbd0a5b3df53fac
callmePluto <anucha.se@rmuti.ac.th>
2024-10-21 19:51:03 +0700

    📌แสดงข้อความประเภทและชื่อผู้ใช้ 📌

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User Dashboard') }} -{{Auth::user()->username}}
+            {{ __('Admin Dashboard') }} -{{Auth::user()->username}}
         </h2>
     </x-slot>
 
