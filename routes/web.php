b5ac291a2d25acb0bc1b4793c29caa31154cc1c7
callmePluto <anucha.se@rmuti.ac.th>
2024-10-22 14:18:08 +0700

    🎨เพิ่ม Layout asset backend🎨

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+
 Route::get('/', function () {
     return view('auth.login');
 });
@@ -15,10 +16,40 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 
-    Route::get('/admin-dashboard', function () {
-        return view('admin-dashboard');
-    })->name('admin.dashboard'); // ตั้งชื่อให้เป็น admin.dashboard
+    // Route::get('/admin-dashboard', function () {
+    //     return view('admin-dashboard');
+    // })->name('admin.dashboard'); // ตั้งชื่อให้เป็น admin.dashboard
+
+    Route::get('/admin-dashboard', [UserController::class, 'index'])->name('admin-dashboard');
+    Route::get('/add-work', function () {
+        return view('add');
+    });
+
+    Route::get('/add-task', function () {
+        return view('project.add');
+    })->name('add-task');
+
+
+    Route::get('/project', function () {
+        return view('projects');
+    })->name('projects');
+
+    Route::get('/account-setting', function () {
+        return view('account_setting');
+    })->name('account_setting');
+
+    Route::get('/personal', function () {
+        return view('personal');
+    })->name('personal');
+
+    Route::get('/addpersonal', function () {
+        return view('addpersonal');
+    })->name('addpersonal');
+
+    Route::get('/system-setting', function () {
+        return view('system-setting');
+    })->name('system-setting');
 });
 
-
-Route::get('/home',[UserController::class,'index']);
+// Route::DELETE('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/home', [UserController::class, 'index']);
