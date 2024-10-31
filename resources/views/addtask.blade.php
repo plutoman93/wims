f56104ec3c484ff6888ec1c615945eb1d575ce29
plutoman93 <anucha.se@rmuti.ac.th>
2024-10-31 17:50:44 +0700

    ลบ 🗑️

--- resources/views/addtask.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.backend.master')
-
-@section('content')
-
-@livewire('addtask')
-
-@endsection
--- resources/views/project/addtask.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.backend.master')
-
-@section('content')
-
-@livewire('project.addtask')
-
-@endsection
