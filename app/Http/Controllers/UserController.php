ae2c6e1472d8291895a3ebc19529aec60537810a
plutoman93 <anucha.se@rmuti.ac.th>
2024-11-17 13:07:16 +0700

    🚧ตรวจสอบสถานะผู้ใช้ (ยังไม่เรียบร้อย)🚧

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,7 +11,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        if (Auth::user()->user_type == 'user') {
+        if (Auth::user()->status && Auth::user()->status->user_status_name === 'user') {
             return redirect()->route('dashboard'); // เปลี่ยนไปที่ dashboard สำหรับ user
         } else {
             return view('admin-dashboard'); // เปลี่ยนไปที่ admin-dashboard สำหรับ admin
