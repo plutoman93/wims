16441d5c4d4e96cbcb9a9c43e0f93ebe3d52a77d
plutoman93 <anucha.se@rmuti.ac.th>
2024-11-17 13:02:06 +0700

    🚧เพิ่ม Seeder (ไว้ทดสอบ) 🚧

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('statuses')->insert([
+            ['user_status_name' => 'user', 'created_at' => now(), 'updated_at' => now()],
+            ['user_status_name' => 'admin', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
