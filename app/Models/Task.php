ec22fd180ef958fd86b912af476a20efcf31f4a2
plutoman93 <anucha.se@rmuti.ac.th>
2024-11-26 19:07:54 +0700

    📝ตั้งค่า user_id ที่อ้างอิง 📝

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\Auth;
 class Task extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected static function booted()
+    {
+        static::creating(function ($task) {
+            // กำหนดค่า task_status_id
+            $task->task_status_id = Task::max('task_status_id') + 1 ?? 1;
+
+            // กำหนดค่า type_id
+            $task->type_id = Task::max('type_id') + 1 ?? 1;
+
+            // กำหนดค่า user_id โดยอ้างอิงว่า user_id ไหนสร้าง task
+            $task->user_id = Auth::id();
+        });
+    }
     public function taskstatus()
     {
         return $this->belongsTo(TaskStatus::class, 'task_status_id', 'task_status_id');
