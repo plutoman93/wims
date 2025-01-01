b02b68b4ac26ed32455109747e278a7ccc26afe0
plutoman93 <anucha.se@rmuti.ac.th>
2025-01-01 14:48:29 +0700

    🚩 ลบไฟล์ที่ไม่จำเป็นและปรับปรุงโค้ดเพื่อเพิ่มประสิทธิภาพและความชัดเจน

--- resources/views/livewire/project/viewtask.blade.php
+++ resources/views/livewire/project/viewtask.blade.php
@@ -1,275 +1,46 @@
 <div>
-    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
         <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>ตารางงาน</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Schedule</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
+            <h1>ตารางงาน</h1>
         </section>
 
-        <!-- Main content -->
         <section class="content">
-
-
-            <!-- Page Wrapper -->
-            <div id="wrapper">
-
-
-                <!-- Content Wrapper -->
-                <div id="content-wrapper" class="d-flex flex-column">
-
-                    <!-- Main Content -->
-                    <div id="content">
-
-                        <!-- Topbar -->
-                        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                            <!-- Sidebar Toggle (Topbar) -->
-                            <form class="form-inline">
-                                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                                    <i class="fa fa-bars"></i>
-                                </button>
-                            </form>
-
-                            <!-- Topbar Search -->
-                            <form
-                                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small"
-                                        placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-
-                            <!-- Topbar Navbar -->
-                            <ul class="navbar-nav ml-auto">
-
-                                <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                                <li class="nav-item dropdown no-arrow d-sm-none">
-                                    <a class="nav-link dropdown-toggle" href="#" id="searchDropdown"
-                                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                        <i class="fas fa-search fa-fw"></i>
-                                    </a>
-                                    <!-- Dropdown - Messages -->
-                                    <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                        aria-labelledby="searchDropdown">
-                                        <form class="form-inline mr-auto w-100 navbar-search">
-                                            <div class="input-group">
-                                                <input type="text" class="form-control bg-light border-0 small"
-                                                    placeholder="Search for..." aria-label="Search"
-                                                    aria-describedby="basic-addon2">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-primary" type="button">
-                                                        <i class="fas fa-search fa-sm"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </li>
-
-
-                                <!-- Nav Item - Messages -->
-                                <li class="nav-item dropdown no-arrow mx-1">
-                                    <!-- Dropdown - Messages -->
-                                    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                        aria-labelledby="messagesDropdown">
-                                        <h6 class="dropdown-header">
-                                            Message Center
-                                        </h6>
-                                        <a class="dropdown-item d-flex align-items-center" href="#">
-                                            <div class="dropdown-list-image mr-3">
-                                                <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                                                    alt="...">
-                                                <div class="status-indicator bg-success"></div>
-                                            </div>
-                                            <div class="font-weight-bold">
-                                                <div class="text-truncate">Hi there! I am wondering if you can help me
-                                                    with a
-                                                    problem I've been having.</div>
-                                                <div class="small text-gray-500">Emily Fowler · 58m</div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item d-flex align-items-center" href="#">
-                                            <div class="dropdown-list-image mr-3">
-                                                <img class="rounded-circle" src="img/undraw_profile_2.svg"
-                                                    alt="...">
-                                                <div class="status-indicator"></div>
-                                            </div>
-                                            <div>
-                                                <div class="text-truncate">I have the photos that you ordered last
-                                                    month, how
-                                                    would you like them sent to you?</div>
-                                                <div class="small text-gray-500">Jae Chun · 1d</div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item d-flex align-items-center" href="#">
-                                            <div class="dropdown-list-image mr-3">
-                                                <img class="rounded-circle" src="img/undraw_profile_3.svg"
-                                                    alt="...">
-                                                <div class="status-indicator bg-warning"></div>
-                                            </div>
-                                            <div>
-                                                <div class="text-truncate">Last month's report looks great, I am very
-                                                    happy with
-                                                    the progress so far, keep up the good work!</div>
-                                                <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item d-flex align-items-center" href="#">
-                                            <div class="dropdown-list-image mr-3">
-                                                <img class="rounded-circle"
-                                                    src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="...">
-                                                <div class="status-indicator bg-success"></div>
-                                            </div>
-                                            <div>
-                                                <div class="text-truncate">Am I a good boy? The reason I ask is because
-                                                    someone
-                                                    told me that people say this to all dogs, even if they aren't
-                                                    good...</div>
-                                                <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item text-center small text-gray-500" href="#">Read
-                                            More Messages</a>
-                                    </div>
-                                </li>
-
-                                <div class="topbar-divider d-none d-sm-block"></div>
-
-                                <!-- Nav Item - User Information -->
-                                <li class="nav-item dropdown no-arrow">
-                                </li>
-
-                            </ul>
-
-                        </nav>
-                        <!-- End of Topbar -->
-
-                        <!-- Begin Page Content -->
-                        <div class="container-fluid">
-                            <!-- DataTales Example -->
-                            <div class="card shadow mb-4">
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered" id="dataTable" width="100%"
-                                            cellspacing="0">
-                                            <thead>
-                                                <tr>
-                                                    <th>ลำดับ</th>
-                                                    <th>ชื่องาน</th>
-                                                    <th>วันเริ่มงาน</th>
-                                                    <th>วันครบกำหนดงาน</th>
-                                                    <th>สถานะงาน</th>
-                                                </tr>
-                                            </thead>
-
-                                            <tbody>
-                                                @if (!empty($data) && $data->isNotEmpty())
-                                                    @foreach ($data as $item)
-                                                        <tr>
-                                                            <td>
-                                                                {{ $item->task_id }}
-                                                            </td>
-                                                            <td>
-                                                                <a>
-                                                                    {{ $item->task_name }}
-                                                                </a>
-                                                            </td>
-                                                            <td>
-                                                                {{ $item->start_date }}
-                                                            </td>
-                                                            <td>
-                                                                {{ $item->due_date }}
-                                                            </td>
-                                                            <td>
-                                                                {{ $item->task_status }}
-                                                            </td>
-                                                            {{-- <td class="project-actions text-right">
-                            <a class="btn btn-primary btn-sm" href="{{route('project-view',['id' =>$item->id])}}">
-                                <i class="fas fa-folder">
-                                </i>
-                                View
-                            </a>
-                            <a class="btn btn-info btn-sm" href="{{route('project-edit',['id' =>$item->id])}}">
-                                <i class="fas fa-pencil-alt">
-                                </i>
-                                Edit
-                            </a>
-                            <a class="btn btn-danger btn-sm" wire:click="delete({{$item->id}})" href="#">
-                                <i class="fas fa-trash">
-                                </i>
-                                Delete
-                            </a>
-                        </td> --}}
-                                                        </tr>
-                                                    @endforeach
-                                                @else
-                                                    <tr>
-                                                        <td colspan="1">No data available</td>
-                                                    </tr>
-                                                @endif
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    {{-- {{ $data->links() }} --}}
-                                    <!-- /.card-body -->
-                                </div>
-
-
-
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
+            <div class="card">
+                <div class="card-header">
+                    <input type="text" class="form-control" placeholder="ค้นหา" wire:model="search">
                 </div>
-                <!-- /.container-fluid -->
-
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr class="text-center">
+                                    <th style="width: 50px;">ลำดับ</th>
+                                    <th>ชื่องาน</th>
+                                    <th style="width: 120px;">วันเริ่มงาน</th>
+                                    <th style="width: 150px;">วันครบกำหนดงาน</th>
+                                    <th style="width: 120px;">สถานะงาน</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($data as $item)
+                                    <tr>
+                                        <td class="text-center">{{ $item->task_id }}</td>
+                                        <td class="text-center">{{ $item->task_name }}</td>
+                                        <td class="text-center">{{ \Carbon\Carbon::parse($item->start_date)->format('d-m-Y') }}</td>
+                                        <td class="text-center">{{ \Carbon\Carbon::parse($item->due_date)->format('d-m-Y') }}</td>
+                                        <td class="text-center">{{ $item->task_status }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5" class="text-center">ไม่พบข้อมูล</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                    {{ $data->links() }} <!-- ใช้ Pagination -->
+                </div>
             </div>
-            <!-- End of Main Content -->
+        </section>
     </div>
-    <!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="js/sb-admin-2.min.js"></script>
-
-<!-- Page level plugins -->
-<script src="vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-<!-- Page level custom scripts -->
-<script src="js/demo/datatables-demo.js"></script>
-
-</section>
-<!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
-
 </div>
