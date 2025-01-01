61c3e24f78a6f2f23444d5b4e55b5e3e8f7d0106
plutoman93 <anucha.se@rmuti.ac.th>
2025-01-01 12:45:52 +0700

    🔖 ลบการนำเข้าโมเดล Status และ Request ใน UserController เพื่อลดความซับซ้อนของโค้ด✅

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Status;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
