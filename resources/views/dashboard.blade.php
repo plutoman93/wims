fc6ac9a33bedb6747fcbc76831ce5554ab9b3a9e
kongzang <gongpipop.2546@gmail.com>
2025-01-11 21:04:20 +0700

    โชว์ข้อมูลdashboard (กำลังทำ)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,9 +36,14 @@
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             งานทั้งหมด</div>
-
+                                            <?php
+                                               $sql = "SELECT COUNT(*) as งานทั้งหมด FROM tasks";
+                                               $query = $conn->prepare($sql);
+                                               $query->execute();
+                                               $fetch = $query->fetch();
+                                            ?>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-
+                                            <?= $fetch['งานทั้งหมด'] ?>
                                     </div>
                                     <div class="col-auto">
                                         <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -56,7 +61,14 @@
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                             งานที่เสร็จแล้ว</div>
+                                            <?php
+                                               $sql = "SELECT COUNT(*) as งานที่เสร็จแล้ว FROM tasks WHERE task_status_id = '1'";
+                                               $query = $conn->prepare($sql);
+                                               $query->execute();
+                                               $fetch = $query->fetch();
+                                            ?>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                                        <?= $fetch['งานทที่เสร็จแล้ว'] ?>
                                     </div>
                                     <div class="col-auto">
                                         <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -72,17 +84,18 @@
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">'งานที่ยังไม่ทำ'
+                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">งานที่ยังไม่ทำ
                                         </div>
+                                        <?php
+                                               $sql = "SELECT COUNT(*) as งานที่ยังไม่ทำ FROM tasks WHERE task_status_id = '2'";
+                                               $query = $conn->prepare($sql);
+                                               $query->execute();
+                                               $fetch = $query->fetch();
+                                            ?>
                                         <div class="row no-gutters align-items-center">
                                             <div class="col-auto">
                                                 <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
-                                            </div>
-                                            <div class="col">
-                                                <div class="progress progress-sm mr-2">
-                                                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
-                                                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
+                                                <?= $fetch['งานที่ยังไม่ทำ'] ?>
                                             </div>
                                         </div>
                                     </div>
@@ -102,7 +115,14 @@
                                     <div class="col mr-2">
                                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                             งานที่กำลังทำ</div>
+                                            <?php
+                                            $sql = "SELECT COUNT(*) as งานที่กำลังทำ FROM tasks WHERE task_status_id = '3'";
+                                            $query = $conn->prepare($sql);
+                                            $query->execute();
+                                            $fetch = $query->fetch();
+                                         ?>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                                        <?= $fetch['งานที่กำลังทำ'] ?>
                                     </div>
                                     <div class="col-auto">
                                         <i class="fas fa-comments fa-2x text-gray-300"></i>
