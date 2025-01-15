2e7e6768b2ee5b7f8094c52d841142835c1462e3
plutoman93 <anucha.se@rmuti.ac.th>
2025-01-15 21:28:52 +0700

    ✨ เปลี่ยนฟังก์ชัน render เป็น searchTasks ใน Livewire component และปรับปรุงการส่งข้อมูลไปยัง view (ยังไม่เสร็จ)🚧

--- app/Livewire/project/TaskPersonal.php
+++ app/Livewire/project/TaskPersonal.php
@@ -28,19 +28,22 @@ class TaskPersonal extends Component
         $model->delete();
     }
 
-    public function render()
+    public function searchTasks()
     {
-        $data = Task::query()
+        return Task::query()
             ->when(Auth::user()->user_status_id !== 1, function ($query) {
                 $query->where('user_id', Auth::id()); // ดูเฉพาะ Task ของตัวเอง ถ้าไม่ใช่ admin
             })
-            ->when($this->search, function($query) {
+            ->when($this->search, function ($query) {
                 $query->where('task_name', 'like', '%' . $this->search . '%');
             })
             ->paginate(10);
+    }
 
+    public function render()
+    {
         return view('livewire.project.viewtask', [
-            'data' => $data,
+            'data' => $this->searchTasks(),
         ]);
     }
 }
