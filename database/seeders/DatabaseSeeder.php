a72d517b06242153044a5c038ab420836f5c8a56
plutoman93 <anucha.se@rmuti.ac.th>
2025-01-15 21:09:01 +0700

    ✨ เพิ่มข้อมูลผู้ใช้ใหม่ใน DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,30 @@ class DatabaseSeeder extends Seeder
                 'department_id' => 1, // ค่า foreign key
                 'user_status_id' => 1, // ค่า foreign key
             ],
+            [
+                'username' => 'random1998',
+                'title_id' => 2, // ค่า foreign key
+                'email' => 'user2@dd.com',
+                'phone' => '0888888888',
+                'password' => bcrypt('22222222220'),
+                'first_name' => 'Random',
+                'last_name' => 'User',
+                'faculty_id' => 3, // ค่า foreign key
+                'department_id' => 1, // ค่า foreign key
+                'user_status_id' => 2, // ค่า foreign key
+            ],
+            [
+                'username' => 'admin1234',
+                'title_id' => 3, // ค่า foreign key
+                'email' => 'user3@dd.com',
+                'phone' => '0777777777',
+                'password' => bcrypt('33333333330'),
+                'first_name' => 'some',
+                'last_name' => 'where',
+                'faculty_id' => 2, // ค่า foreign key
+                'department_id' => 2, // ค่า foreign key
+                'user_status_id' => 2, // ค่า foreign key
+            ],
         ]);
     }
 }
