605d8446839028c86e4d051e1c19e9f5ad3a5a44
plutoman93 <anucha.se@rmuti.ac.th>
2025-01-17 23:05:09 +0700

    ✨ ปรับปรุงการค้นหาใน TaskPersonal เพื่อให้ทำงานเฉพาะเมื่อมีการป้อนข้อมูลค้นหา

--- app/Livewire/project/TaskPersonal.php
+++ app/Livewire/project/TaskPersonal.php
@@ -34,7 +34,9 @@ class TaskPersonal extends Component
             ->when(Auth::user()->user_status_id !== 1, function ($query) {
                 $query->where('user_id', Auth::id()); // ดูเฉพาะ Task ของตัวเอง ถ้าไม่ใช่ admin
             })
-            ->where('task_name', 'like', '%' . $this->search . '%')
+            ->when($this->search, function ($query) {
+                $query->where('task_name', 'like', '%' . $this->search . '%');
+            })
             ->paginate(10); // ใช้ Pagination
 
         return view('livewire.project.viewtask', compact('data'));
