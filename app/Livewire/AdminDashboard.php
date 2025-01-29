c0a501f9d199f2b8946678c16735133d5a9a62d4
kongzang <gongpipop.2546@gmail.com>
2025-01-29 17:00:38 +0700

    ✨ ปรับปรุงฟังก์ชันการนับงานใน Admin Dashboard โดยรวมการนับงานเสร็จสิ้นและยังไม่เสร็จเข้าด้วยกัน 🎨

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -14,24 +14,12 @@ class AdminDashboard extends Component
     public function taskCount()
     {
         $this->count = DB::table('tasks')->count();
-    }
-
-    public function taskCompletedCount()
-    {
         $this->countCompleted = DB::table('tasks')->where('task_status_id', 1)->count();
-    }
-
-    public function taskUncompletedCount()
-    {
         $this->countUncompleted = DB::table('tasks')->where('task_status_id', 2)->count();
     }
-
     public function render()
     {
         $this->taskCount();
-        $this->taskCompletedCount();
-        $this->taskUncompletedCount();
-        // You can call taskCount or taskCompletedCount here if needed
         return view('livewire.admin-dashboard', ['count' => $this->count]);
     }
 }
