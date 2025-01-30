f274a0879d345d05ca38aa87ce9a86f5d6861c22
kongzang <gongpipop.2546@gmail.com>
2025-01-30 16:07:52 +0700

    ✨ แก้ไขการสะกดคำในหัวข้อสถานะงานใน Admin Dashboard 🎨

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -95,7 +95,7 @@
                     <div class="card shadow mb-4">
                         <!-- Card Header -->
                         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                            <h6 class="m-0 font-weight-bold text-primary">สถาณะงาน</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">สถานะงาน</h6>
                         </div>
                         <!-- Card Body -->
                         <div class="card-body">
