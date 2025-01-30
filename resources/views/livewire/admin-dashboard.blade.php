231c36af01389b1fae1de10ddd2b0043d8da84fc
kongzang <gongpipop.2546@gmail.com>
2025-01-30 15:51:12 +0700

    ✨ ปรับปรุงการแสดงผลจำนวนงานที่กำลังทำใน Admin Dashboard 🎨

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -65,30 +65,6 @@
                     </div>
                 </div>
 
-                <!-- Earnings (Monthly) Card Example -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-left-info shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                        งานที่ยังไม่เสร็จ
-                                    </div>
-                                    <h1 style="text-align: center;"> {{ $countUncompleted }}</h1>
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <!-- Pending Requests Card Example -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-warning shadow h-100 py-2">
@@ -97,7 +73,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                         งานที่กำลังทำ</div>
-                                    <h1 style="text-align: center;">{{ $tasksInProgress }}</h1>
+                                    <h1 style="text-align: center;">{{ $countUncompleted }}</h1>
 
                                     <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                 </div>
