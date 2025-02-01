bac71ac780aea3e6e131a8f426fe6f8c493a81c1
plutoman93 <anucha.se@rmuti.ac.th>
2025-02-01 17:45:04 +0700

    ✨ ปรับปรุงฟังก์ชันการกู้คืนผู้ใช้และงาน พร้อมเพิ่มฟังก์ชันรีเฟรชข้อมูลงานที่ถูกลบ 🎉 (ดีขึ้นนิดหน่อย)

--- app/Livewire/Restore.php
+++ app/Livewire/Restore.php
@@ -10,25 +10,19 @@ class Restore extends Component
 {
     public $users, $tasks;
 
-    public function mount()
+    public function mount() // โหลดข้อมูล User และ Task ที่ถูกลบ
     {
         $this->users = collect();
         $this->tasks = collect();
         $this->showTrash();
     }
 
-    public function showTrash()
+    public function showTrash() // แสดงข้อมูล User และ Task ที่ถูกลบ
     {
         $this->users = User::onlyTrashed()->get();
         $this->tasks = Task::onlyTrashed()->get();
 
         // return view('livewire.restore')->with('users', $this->users)->with('tasks', $this->tasks);
-    }
-
-    public function deleteUser()
-    {
-        $user = User::onlyTrashed()->get();
-        $user->forceDelete();
     }
 
     public function restoreUser($id)
@@ -42,12 +36,17 @@ class Restore extends Component
     {
         $task = Task::onlyTrashed()->find($id);
         $task->restore();
-        $this->refreshUsers();
+        $this->refreshTasks();
     }
 
     public function refreshUsers()
     {
         $this->users = User::onlyTrashed()->get(); // โหลดข้อมูล User ที่ถูกลบ
+    }
+
+    public function refreshTasks()
+    {
+        $this->tasks = Task::onlyTrashed()->get(); // โหลดข้อมูล Task ที่ถูกลบ
     }
 
     public function render()
