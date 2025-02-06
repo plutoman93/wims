39c7d6c8bfce6d756226b208e0938ab240557ce2
kongzang <gongpipop.2546@gmail.com>
2025-02-06 14:55:31 +0700

    🎨 ปรับปรุงลิงก์ "Home" ในแดชบอร์ดและหน้าต่างๆ เพื่อความชัดเจนในการนำทาง 📝

--- resources/views/livewire/project/edit.blade.php
+++ resources/views/livewire/project/edit.blade.php
@@ -5,7 +5,7 @@
                 <div class="row mb-2">
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a>Home</a></li>
                             <li class="breadcrumb-item active">แก้ไขข้อมูลบุคลากร</li>
                         </ol>
                     </div>
--- resources/views/livewire/project/view.blade.php
+++ resources/views/livewire/project/view.blade.php
@@ -8,7 +8,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a>Home</a></li>
                         <li class="breadcrumb-item active">User Profile</li>
                     </ol>
                 </div>
@@ -27,7 +27,7 @@
                         <div class="card-body box-profile">
                             <div class="text-center">
                                 <img class="profile-user-img img-fluid img-circle"
-                                    src="https://ui-avatars.com/api/?name={{ $first_name }} {{ $last_name }}" 
+                                    src="https://ui-avatars.com/api/?name={{ $first_name }} {{ $last_name }}"
                                     alt="User profile picture">
                             </div>
 
