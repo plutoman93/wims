df07929408e9a8774d3786661a7586c0d2231c94
callmePluto <anucha.se@rmuti.ac.th>
2025-02-24 18:51:50 +0700

    🎨 ปรับปรุงประเภทงานใน seeder โดยเปลี่ยนชื่อประเภทงานและเพิ่มประเภทใหม่

--- database/seeders/TaskTypeSeeder.php
+++ database/seeders/TaskTypeSeeder.php
@@ -16,8 +16,10 @@ class TaskTypeSeeder extends Seeder
         DB::table('task_types')->truncate();
         DB::table('task_types')->insert([
             ['type_id' => 1, 'type_name' => 'ปฏิบัติราชการ'],
-            ['type_id' => 2, 'type_name' => 'ลากิจ'],
-            ['type_id' => 3, 'type_name' => 'ประชุม'],
+            ['type_id' => 2, 'type_name' => 'ลาป่วย'],
+            ['type_id' => 3, 'type_name' => 'ลาพักผ่อน'],
+            ['type_id' => 4, 'type_name' => 'ประชุม'],
+            ['type_id' => 5, 'type_name' => 'อบรม'],
         ]);
     }
 }
