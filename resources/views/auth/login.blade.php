9dfbc2a89b5b5707a4c32de375421949cba8e0c9
callmePluto <anucha.se@rmuti.ac.th>
2025-03-11 13:50:23 +0700

    📝 ลบสคริปต์ Google Map ออกจากหน้าเข้าสู่ระบบเพื่อลดการโหลดทรัพยากร

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -101,9 +101,6 @@
     <script src="asset/frontend/plugins/bootstrap/js/bootstrap.min.js"></script>
     <!-- Slick Slider -->
     <script src="asset/frontend/plugins/slick-carousel/slick.min.js"></script>
-    <!-- Google Map -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
-    <script src="asset/frontend/plugins/google-map/gmap.js"></script>
     <!-- main js -->
     <script src="asset/frontend/js/custom.js"></script>
 
